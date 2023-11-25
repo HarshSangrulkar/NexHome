a83e454ad98a5d6a67fb6a55ff2e4b1a01e8b6df
Harsh Sangrulkar <harshsangrulkar@gmail.com>
2023-11-25 16:20:58 +0530

    path 2

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const path = require("path");
 
 mongoose.set("strictQuery", true);
 mongoose.connect(
-  // "mongodb+srv://harshsangrulkar:1234@cluster0.8ptiwxj.mongodb.net/?retryWrites=true&w=majority"
   "mongodb+srv://purnasaikrishnainnamuri23:12345@cluster0.q5cu4tp.mongodb.net/NexHome?retryWrites=true&w=majority"
 );
 
@@ -18,8 +17,9 @@ var db = mongoose.connection;
 db.on("open", () => {
   console.log("Connected to DB");
 });
+
 db.on("error", () => {
-  console.log("error occured");
+  console.log("Error occurred while connecting to the database");
 });
 
 const app = express();
@@ -28,14 +28,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
+
+// Serve static files from the 'client/dist' directory
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+// Define your routes
 app.use("/nexHome", nexHomeRouter);
 app.use("/sell", sellRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
+// For any other route, serve the index.html file
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
-app.listen(4000, () => {
-  console.log("started at 4000");
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
